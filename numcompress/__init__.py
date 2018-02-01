6f4dcb5f1e1a720bbf633a2d6e397727b0e19777
Amit Rathi <amit@hypertrack.io>
2018-02-01 16:04:19 +0530

    Setup, .gitignore etc.

---
+++ numcompress/__init__.py
@@ -0,0 +1,7 @@
+""" numcompress/__init__.py """
+
+# __all__ = []
+
+from .numcompress import *
+
+from .version import __version__
---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='numcompress',
+    packages=['numcompress'],
+    version='0.1.1',
+    description='Python package to compress numerical series into strings',
+    author='Amit Rathi',
+    author_email='amit.juschill@gmail.com',
+    url='https://github.com/amit1rrr/numcompress',
+    license='MIT',
+    download_url='',
+    keywords=[],
+    classifiers=[]
+)
